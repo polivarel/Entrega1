63a497d89879a6fd00d833d5ca9361d38b53f720
polivarel <p.olivare@outlook.com>
2022-09-05 20:37:10 -0300

    Entrega 1, Base creada 2

--- Apps_Entrega1/models.py
+++ Apps_Entrega1/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 
-class eventos(models.Model):
-    fecha_inicio=models.CharField(max_length=10,blank=True,null=True)
+class peliculas(models.Model):
+    fecha_inicio=models.DateField(max_length=10,blank=True,null=True)
     lugar       =models.CharField(max_length=50,blank=True,null=True)
-    tipo        =models.CharField(max_length=30,blank=True,null=True)
     titulo      =models.CharField(max_length=50,blank=True,null=True)
-    hora_inicio =models.CharField(max_length=5,blank=True,null=True)
-    aforo       =models.CharField(max_length=50,blank=True,null=True)
-    genero      =models.CharField(max_length=50,blank=True,null=True)
-    edad_minima =models.CharField(max_length=50,blank=True,null=True)
-    duracion    =models.CharField(max_length=50,blank=True,null=True)
-    puntaje     =models.CharField(max_length=50,blank=True,null=True)
-    elenco      =models.CharField(max_length=50,blank=True,null=True)
-    sector      =models.CharField(max_length=50,blank=True,null=True)
-    equipos     =models.CharField(max_length=500,blank=True,null=True)
-    deporte     =models.CharField(max_length=50,blank=True,null=True)
-    contacto    =models.CharField(max_length=1000,blank=True,null=True)
+    hora_inicio =models.TimeField(max_length=5,blank=True,null=True)
+    edad_minima =models.IntegerField(max_length=50,blank=True,null=True)
+    puntaje     =models.IntegerField(max_length=50,blank=True,null=True)
+    
+class teatro(models.Model):
+    fecha_inicio=models.DateField(max_length=10,blank=True,null=True)
+    lugar       =models.CharField(max_length=50,blank=True,null=True)
+    titulo      =models.CharField(max_length=50,blank=True,null=True)
+    hora_inicio =models.TimeField(max_length=5,blank=True,null=True)
+    elenco     =models.CharField(max_length=1000,blank=True,null=True)
+
+class deporte(models.Model):
+    fecha_inicio=models.DateField(max_length=10,blank=True,null=True)
+    lugar       =models.CharField(max_length=50,blank=True,null=True)
+    titulo      =models.CharField(max_length=50,blank=True,null=True)
+    hora_inicio =models.TimeField(max_length=5,blank=True,null=True)
+    equipo1     =models.CharField(max_length=100,blank=True,null=True)
+    equipo2     =models.CharField(max_length=100,blank=True,null=True)
+    deporte     =models.CharField(max_length=100,blank=True,null=True)
